7c7e04d03da8828a742386be494d5f74a26f9231
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-10 11:23:56 +1000

    added logos in

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,5 +1,15 @@
 import './index.scss'
+import { Link } from 'react-router-dom'
+import MainLogo from '../../assets/images/EL-LOGO.png'
+import SubLogo from '../../assets/images/ELSIE-SUB-LOGO.png'
 
-const Sidebar = () => <h1>Hello World SIdebar</h1>
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={MainLogo} alt="logo" />
+      <img src={SubLogo} alt="subtitlelogo" />
+    </Link>
+  </div>
+)
 
 export default Sidebar
