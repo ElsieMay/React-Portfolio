c07122772e3f8a1d4d1e90274d39bb714363d467
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-10 23:32:11 +1000

    added styling for cube elements

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -25,7 +25,7 @@ const About = () => {
           />
         </h1>
         <p>
-          I am a junior full stack developer, with a background in visual and
+          I am a junior full stack developer with a background in visual and
           fashion design. I am a passionate creative, driven by fairness and
           equity, with respect for individual expression and authentic
           perspectives. Using strategy, discovery and collaboration, I am driven
