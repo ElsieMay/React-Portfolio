cc1ec37447938802fbb78c26710dde77fed33de9
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-10 23:16:02 +1000

    added animation to about me title

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,15 @@
 import AnimatedLetters from '../AnimatedLetters'
+import { useEffect, useState } from 'react'
 import './index.scss'
 
 const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
   return (
     <div className="container about-page">
       <div className="text-area">
         <h1>
           <AnimatedLetters
-            // letterClass={letterClass}
+            letterClass={letterClass}
             strArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
             idx={15}
           />
