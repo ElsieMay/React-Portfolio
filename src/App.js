197325f7dfb1497e22127df5891e97f4b0d9674d
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-10 14:39:37 +1000

    added styling for html tags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-// Imports App sass file
-import { Routes, Route } from 'react-router-dom'
-import './App.scss'
+import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
+import './App.scss'
 
 function App() {
   return (
