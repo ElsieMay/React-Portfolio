6091a33b99c671658589e92db35062aeedcda6d9
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-10 21:40:44 +1000

    added about route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
+import About from './components/About'
 import './App.scss'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/react-portfolio" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/react-portfolio/about" element={<About />} />
         </Route>
       </Routes>
     </>
