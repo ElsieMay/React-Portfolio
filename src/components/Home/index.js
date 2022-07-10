0c6d8271dea5621d431f8dc39c187923451f000f
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-10 14:50:42 +1000

    added logo for name

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,3 +1,16 @@
-const Home = () => 'home'
+import Logo from '../../assets/images/EL-LOGO.png'
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-area">
+        <h1>
+          Hi, <br /> I'm
+        </h1>
+        <img src={Logo} alt="developername" />
+      </div>
+    </div>
+  )
+}
 
 export default Home
