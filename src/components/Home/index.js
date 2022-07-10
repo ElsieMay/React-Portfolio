07b6386bf5785b95a21935853553f31e54db7507
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-10 19:25:04 +1000

    added styling for contact button

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,7 +15,7 @@ const Home = () => {
         </h1>
         <h2>Digital Designer || Javascript Enthusiast </h2>
         <Link to="/contact" className="flat-button">
-          Contact Me
+          CONTACT ME
         </Link>
       </div>
     </div>
