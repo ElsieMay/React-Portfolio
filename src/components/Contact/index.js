8aa14e886be6cc81ac43de81fe9631a5cb50c370
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-13 22:33:09 +1000

    added title to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,24 @@
 import './index.scss'
+import { useState } from 'react'
+import AnimatedLetters from '../AnimatedLetters'
 
-const Contact = () => 'Hello'
+const Contact = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="text-area">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'M', 'e']}
+              idx={15}
+            />
+          </h1>
+        </div>
+      </div>
+    </>
+  )
+}
 
 export default Contact
