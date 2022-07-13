e5f7e6744e9424aa7381d546dada36c2a7ac5a72
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-13 22:41:35 +1000

    added contact form to contact page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -15,6 +15,50 @@ const Contact = () => {
               idx={15}
             />
           </h1>
+          <p>
+            It would be lovely to hear from you. Feel free to drop a message
+            below.
+          </p>
+          <div className="form-contact">
+            <form>
+              <ul>
+                <li className="first-half">
+                  <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    required
+                  ></input>
+                </li>
+                <li className="first-half">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    required
+                  ></input>
+                </li>
+                <li>
+                  <input
+                    type="text"
+                    name="subject"
+                    placeholder="Subject"
+                    required
+                  ></input>
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="Send" />
+                </li>
+              </ul>
+            </form>
+          </div>
         </div>
       </div>
     </>
