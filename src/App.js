c33e22e86c251d3244e33d2e26c3256080df6340
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-13 22:18:03 +1000

    added Contact folder and files

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
+import Contact from './components/Contact'
 import './App.scss'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/react-portfolio" element={<Layout />}>
           <Route path="/react-portfolio" index element={<Home />} />
           <Route path="/react-portfolio/about" element={<About />} />
+          <Route path="/react-portfolio/contact" element={<Contact />} />
         </Route>
       </Routes>
     </>
