5122cebab9ff59784ecfaae45caaea3931c925fe
ElsieMay <elsiemaylawrie1@gmail.com>
2022-07-14 14:07:33 +1000

    added route for portfolio page, added three js index file to correct folder

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from './components/Layout'
 import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
 import './App.scss'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
           <Route path="/react-portfolio" index element={<Home />} />
           <Route path="/react-portfolio/about" element={<About />} />
           <Route path="/react-portfolio/contact" element={<Contact />} />
+          <Route path="/react-portfolio/portfolio" element={<Portfolio />} />
         </Route>
       </Routes>
     </>
